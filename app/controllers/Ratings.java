ac8e25bfbfb009a176d5f429387eb434ff78bea9
foo4foo <stefannn.live@live.com>
2017-06-16 23:57:34 +0200

    new view added

--- app/controllers/Ratings.java
+++ app/controllers/Ratings.java
@@ -6,15 +6,44 @@ import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationExceptio
 import play.db.DB;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.visits;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 /**
  * Created by stefan on 6/16/17.
  */
 public class Ratings extends Controller {
+
+    public static Result visits() {
+        try (Connection connection = DB.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "Select r.restaurantId, r.name, r.address, r.description from restaurants as r " +
+                            "left join visits as v on r.restaurantId = v.restaurantId left join users as u " +
+                            "on v.userId = u.userId where u.email = (Select email from users where email = ?);");
+
+            preparedStatement.setString(1, session("connected"));
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1));
+                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getString(3));
+                System.out.println(resultSet.getString(4));
+            }
+
+
+            return ok(visits.render(session("connected")));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return badRequest("Nesto se cudno desilo");
+        }
+    }
 
     public static Result visitInfo(){
         JsonNode ajax_json = request().body().asJson();
@@ -23,6 +52,7 @@ public class Ratings extends Controller {
 
     public static Result rateRestaurantAJAX() {
         try (Connection connection = DB.getConnection()) {
+            connection.setAutoCommit(false);
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode ajax_json = request().body().asJson();
             HashMap<String, String> response = objectMapper.convertValue(ajax_json, HashMap.class);
