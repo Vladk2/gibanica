4d73a985f98bed5a3939e47f09915662313aad9e
foo4foo <stefannn.live@live.com>
2017-06-17 00:21:07 +0200

    guest visits are now in separate files

--- app/controllers/Ratings.java
+++ app/controllers/Ratings.java
@@ -3,6 +3,7 @@ package controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+import models.Restaurant;
 import play.db.DB;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -12,7 +13,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by stefan on 6/16/17.
@@ -20,29 +23,39 @@ import java.util.HashMap;
 public class Ratings extends Controller {
 
     public static Result visits() {
-        try (Connection connection = DB.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "Select r.restaurantId, r.name, r.address, r.description from restaurants as r " +
-                            "left join visits as v on r.restaurantId = v.restaurantId left join users as u " +
-                            "on v.userId = u.userId where u.email = (Select email from users where email = ?);");
+        if(session("userType").equals("guest")) {
+            try (Connection connection = DB.getConnection()) {
+                /* PORED PODATAKA O RESTARANU, TREBA DA SE IZLISTAJU I PODACI O POSETI (to jos nije uradjeno) */
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "Select r.restaurantId, r.name, r.description, r.address, r.tel, r.size from restaurants as r " +
+                                "left join visits as v on r.restaurantId = v.restaurantId left join users as u " +
+                                "on v.userId = u.userId where u.email = (Select email from users where email = ?);");
 
-            preparedStatement.setString(1, session("connected"));
+                preparedStatement.setString(1, session("connected"));
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+                ResultSet resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1));
-                System.out.println(resultSet.getString(2));
-                System.out.println(resultSet.getString(3));
-                System.out.println(resultSet.getString(4));
+                List<Restaurant> restaurants = new ArrayList<Restaurant>();
+
+                while (resultSet.next()) {
+                    restaurants.add(new Restaurant(
+                            resultSet.getString(1),
+                            resultSet.getString(2),
+                            resultSet.getString(3),
+                            resultSet.getString(4),
+                            resultSet.getString(5),
+                            resultSet.getString(6)
+                    ));
+                }
+
+
+                return ok(visits.render(session("connected"), restaurants));
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return badRequest("Nesto se cudno desilo");
             }
-
-
-            return ok(visits.render(session("connected")));
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return badRequest("Nesto se cudno desilo");
         }
+        return badRequest("Vi niste gost");
     }
 
     public static Result visitInfo(){
